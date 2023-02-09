1a2b111f2eff1d7c58a802d444e653f43dfc3605
professorsugoi <emosock@live.com>
2023-02-09 13:28:06 -0500

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,39 @@
+import styles from '../styles/NFTCard.module.css';
+import { NFTCard } from '../components/nftCard';
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+	const [NFTs, setNFTs] = useState([]);
+
+	useEffect(() => {
+		fetchNFTs();
+	}, []);
+
+	// Fetches all NFTs in a collection
+	const fetchNFTs = async () => {
+		var requestOptions = {
+			method: 'GET',
+		};
+		const api_key = 'bw97-06c_zYVXE0U9BnEeGw2a2SvELFP';
+		const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
+		const fetchURL = `${baseURL}?contractAddress=0x231EDa2D0E36E5254515B7625D1201f7b4617cf4&withMetadata=${'true'}`;
+		const nfts = await fetch(fetchURL, requestOptions).then((data) =>
+			data.json()
+		);
+		if (nfts) {
+			console.log('NFTs in collection:', nfts);
+			setNFTs(nfts.nfts);
+		}
+	};
+
+	return (
+		<div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
+			{NFTs.length &&
+				NFTs.map((nft) => {
+					return <NFTCard nft={nft}></NFTCard>;
+				})}
+		</div>
+	);
+};
+
+export default Home;
