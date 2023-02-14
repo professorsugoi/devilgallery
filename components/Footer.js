4a7f7c01936733060ef25631c775d0e510a1a49a
professorsugoi <emosock@live.com>
2023-02-14 18:47:05 -0500

    removed tailwind

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = ({}) => {
+	return (
+		<footer>
+			<a href='#' className='logo'>
+				<h2>DEVILISH</h2>
+			</a>
+			<a
+				href='#'
+				className='signature'
+				aria-label='sugoi signature, click to visit website'
+			>
+				<img src='/comfi.webp' alt='comfi image' />
+			</a>
+		</footer>
+	);
+};
+
+export default Footer;
